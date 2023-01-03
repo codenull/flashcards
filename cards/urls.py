9820893570a1e6802534f3cec2e20f38e145f5ff
CodeNull <code.null@yandex.ru>
2023-01-04 01:34:41 +1000

    Заглушка под будующие urls

---
+++ cards/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+urlpatterns = [
+    # path('', TemplateView.as_view(template_name='cards/base.html'), name='home'),
+    # path('', CardListView.as_view(), name='card-list'),
+    # path('new', CardCreateView.as_view(), name='card-create'),
+    # path('edit/<int:pk>', CardUpdateView.as_view(), name='card-update'),
+    # path('box/<int:box_num>', BoxView.as_view(), name='box'),
+]
