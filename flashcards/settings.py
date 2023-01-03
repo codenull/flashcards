f1621c5ba8408933843663047e57e0189ba7c09c
CodeNull <code.null@yandex.ru>
2023-01-03 23:28:48 +1000

    Пофиксил проблему загрузки environment которая возникала при makemigrations.

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -13,12 +13,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import environ
 
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cards',
 ]
 
 MIDDLEWARE = [
